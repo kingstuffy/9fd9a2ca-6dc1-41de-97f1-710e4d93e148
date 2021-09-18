c8dca08595459c8d122fcf0d15cda1e0b88c175e
Olawale Malomo <warlay234@gmail.com>
2021-09-18 22:02:07 +0100

    Add average rating to product endpoint

--- api/controllers/product/read.js
+++ api/controllers/product/read.js
@@ -12,21 +12,31 @@ module.exports = {
         'data': {
           'reviews': [
             {
-              'createdAt': 1631994010280,
-              'updatedAt': 1631994010280,
-              'id': '6146409a56d629467ef3fa34',
+              'createdAt': 1631998872644,
+              'updatedAt': 1631998872644,
+              'id': '6146539848252152ccdf626f',
+              'text': 'Some ratings text',
+              'rating': 3,
+              'isDeleted': false,
+              'product': '614642ae3fb280499cebf6df'
+            },
+            {
+              'createdAt': 1631998866750,
+              'updatedAt': 1631998866750,
+              'id': '6146539248252152ccdf626e',
               'text': 'Some ratings text',
               'rating': 4,
               'isDeleted': false,
-              'product': '614637af8e3f5b41e0136155'
+              'product': '614642ae3fb280499cebf6df'
             }
           ],
-          'createdAt': 1631991727861,
-          'updatedAt': 1631991727861,
-          'id': '614637af8e3f5b41e0136155',
+          'createdAt': 1631994542875,
+          'updatedAt': 1631994542875,
+          'id': '614642ae3fb280499cebf6df',
           'name': 'The Minimalist Entrepreneur',
           'slug': 'default',
-          'isDeleted': false
+          'isDeleted': false,
+          'averageRating': 3.5
         }
       },
     },
@@ -49,9 +59,14 @@ module.exports = {
       return exits.notFound({ message: 'Product not found' });
     }
 
+    const averageRating = await Review.avg('rating')
+      .where({
+        product: product.id
+      });
+
     const response = {
       message: 'Product retrieved successfully',
-      data: product,
+      data: { ...product, averageRating },
     };
     return exits.success(response);
   }
